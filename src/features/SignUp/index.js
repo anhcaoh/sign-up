68d74f3b5d777ce6d49f78a4c54362b17b4686a2
Anh Cao <anhcaoh@gmail.com>
2021-01-07 17:47:30 -0500

    Added UX visual indicators success vs warning

--- src/features/SignUp/index.js
+++ src/features/SignUp/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import { setElements, reset } from "Store/actions";
 import { Columns, Column, Row, Block, Flexbox } from "Components/Layout";
@@ -18,6 +18,7 @@ function SignUp( props ) {
     const [ isValidForm, setIsValidForm ] = useState( false );
     const [ hasAnyFieldValue, setHasAnyFieldValue ] = useState( false );
     const [ isSubmitted, setSubmission ] = useState( false );
+
     const checkCustomValidity = ( e, obj ) => {
         let _obj = Object.assign({}, obj);
         if( obj.id === "passwordConfirm" ){
@@ -37,26 +38,28 @@ function SignUp( props ) {
         
         let currentElOfElems = null;
         const _validity = (e.target && e.target.validity) || {};
-        const _elements = 
-        ((elements &&
-        elements.length) && //null||undefined check
-        elements.map( el => {
-            if( el.id === "passwordConfirm" && (el.id === obj.id) ){
-                const _obj = checkCustomValidity(e, obj);
-                el.value = obj.value;
-                el.valid = _obj.valid;
-                e.target.classList.toggle( "invalid", (!_obj.valid) );
-            } else if (el.id === obj.id) {
-                currentElOfElems = Object.assign({}, el);//no reference
-                el.value = obj.value;
-                el.valid = _validity.valid;
-                e.target.classList.toggle( "invalid", (!_validity.valid) );
-            }
-        return el;
-        })) || [];
+        const _elementsWithValue = 
+                ((elements &&
+                elements.length) && //null||undefined check
+                elements.map( el => {
+                    if( el.id === "passwordConfirm" && (el.id === obj.id) ){
+                        const _obj = checkCustomValidity(e, obj);
+                        el.value = obj.value;
+                        el.valid = _obj.valid;
+                        e.target.classList.toggle( "invalid", !_obj.valid );
+                        e.target.classList.toggle( "valid", _obj.valid );
+                    } else if (el.id === obj.id) {
+                        currentElOfElems = Object.assign({}, el);//no reference
+                        el.value = obj.value;
+                        el.valid = _validity.valid;
+                        e.target.classList.toggle( "invalid", !_validity.valid );
+                        e.target.classList.toggle( "valid", _validity.valid );
+                    }
+                return el;
+                })) || [];
         if ( currentElOfElems && 
             (currentElOfElems.value !== e.target.value) ) {
-            _setElems(_elements);
+            _setElems(_elementsWithValue); //setElems when value changed
         }
         const _isValidForm = e.target.form.checkValidity();
         setIsValidForm(_isValidForm);
@@ -72,7 +75,7 @@ function SignUp( props ) {
 
     const hasFieldsValue = ( ) => {
         const anyFieldsValue = elements.map(el => !!el.value).filter(i => i);
-        setHasAnyFieldValue( anyFieldsValue );
+        setHasAnyFieldValue( anyFieldsValue.length ); //if any value, set hasAnyFieldValue 
     };
     
     const handleReset = ( e ) => {
@@ -94,9 +97,7 @@ function SignUp( props ) {
             <H2 className="margin-top--0">{heading}</H2>
             {isSubmitted ? <Label className="label--success">
             Congratulations! You have successfully submitted as a new user.
-            </Label> : 
-            <Label>{label}</Label>
-            }
+            </Label> : <Label>{label}</Label>}
             <Block className="vertical-space--2 margin-top--2">
                 { ( elements && 
                     elements.length && (
@@ -105,6 +106,7 @@ function SignUp( props ) {
                         <Label>{elem.label}</Label>
                         <Input id={ elem.id || null }
                         type={ elem.type || "text" }
+                        inputType={ elem.inputType || "text" }
                         autoFocus={ index === 0 ? true : false }
                         placeholder={ elem.placeholder || null }
                         title={ elem.title || null }
@@ -117,7 +119,8 @@ function SignUp( props ) {
                         <Label className={
                             ["text--small", 
                             (elem.valid === false) ? 
-                            "label--warning" : "label--default"].join(" ").trim()}>
+                            "label--warning" : 
+                            "label--default"].join(" ").trim()}>
                         {(elem.valid === false) ? 
                         elem.validityMessage : "" }</Label>
                         </Block>);    
@@ -128,13 +131,13 @@ function SignUp( props ) {
                 <Button className="clear" 
                 disabled={!hasAnyFieldValue || isSubmitted}
                 onClick={ handleReset }>
-                    <Text>Reset</Text>
+                    <Text className="uppercase">Reset</Text>
                 </Button>
                 <Button type="submit" 
                 className="primary float--right" 
                 disabled={!isValidForm || isSubmitted }>
                     <Text className="uppercase">
-                        { isSubmitted ? "Submitted" : "Submit" }
+                    { isSubmitted ? "Submitted" : "Submit" }
                     </Text>
                 </Button>
             </Block>
